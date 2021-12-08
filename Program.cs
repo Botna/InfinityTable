476d621f4df25377e7b617fe25c440b32dac0097
Botna <drewzisa@gmail.com>
2021-12-08 11:49:39 -0800

    updated program and added script

--- Program.cs
+++ Program.cs
@@ -1,5 +1,9 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using PiLed.Devices.Implementations;
+using PiLed.Display;
+using PiLed.Models;
+using System;
 
 namespace infinityTableWebsite
 {
@@ -7,7 +11,15 @@ namespace infinityTableWebsite
     {
         public static void Main(string[] args)
         {
+            try
+            {
+                SetupPiLeds();
+            }
+            catch (Exception ex)
+            { }
+
             CreateHostBuilder(args).Build().Run();
+            
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -16,5 +28,15 @@ namespace infinityTableWebsite
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+        public static void SetupPiLeds()
+        {
+            var numLeds = 50;
+            var device = new WS2801PixelDevice(new PiLed.Devices.Config.PixelConfig() { FlushRate = 10, NumLeds = numLeds });
+            //var demoMode = new DemoMode(device);
+            //demoMode.Start();
+            var singleColorMode = new SolidColorDisplay(device, new PixelColor(0, 1, 1));
+            singleColorMode.Start();
+        }
     }
 }
